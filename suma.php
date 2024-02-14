894a34fd97d983499dd77649090fb678397fc6ba
chava-PA <159972658+chava-PA@users.noreply.github.com>
2024-02-13 19:26:32 -0600

    Add files via upload

---
+++ suma.php
@@ -0,0 +1,37 @@
+<html>
+<head></head>
+<body>
+<h1>12662. C16</h1>
+
+<h2>Descripción</h2>
+OMIJal se ha encontrado con una variante
+ en el Concurso ACM, pues el numero de programadores que conforman
+  un equipo puede variar, entonces modifica tu programa anterior pues 
+  ahora deberás preguntar el numero de 3 integrantes por equipo, al final 
+  indica cuantos equipos puedenformarse y cuantos programadores quedan fuera
+
+<h2>Entrada</h2>
+Dos números que representen el número de programadores
+ y el número de programadores por equipo
+
+<h2>Salida</h2>
+Salida con el número de equipos formados y los estudiantes sin equipo,
+ escrito exactamente como en el ejemplo
+
+<h2>Ejemplo</h2>
+
+<?php
+# CODIGO PHP PARA LA MULTIPLICACION
+$a=18;
+$b=3;
+
+# OPERACION
+$m=$a/$b;
+$v=$a%$b;
+echo "<br>equipos formado :", $m;
+echo "<br>estudiantes sin equuipo :", $v;
+?>
+
+
+</body>
+</html>
